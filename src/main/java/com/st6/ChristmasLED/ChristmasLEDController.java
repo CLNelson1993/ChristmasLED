fa5e65807b632b65bddf8cc7a5564138dd0e536f
CLNelson1993 <143137154+CLNelson1993@users.noreply.github.com>
2023-11-04 02:53:20 -0700

    Update ChristmasLEDController.java

--- src/main/java/com/st6/ChristmasLED/ChristmasLEDController.java
+++ src/main/java/com/st6/ChristmasLED/ChristmasLEDController.java
@@ -1,3 +1,5 @@
+//Tested by running on Windows 10 and doing localhost:8080/test/100.
+//this definitely works, so it's safe to say the Pi isn't properly set up for Spring-boot/tomcat9. 
 package com.st6.ChristmasLED;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -6,9 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ChristmasLEDController {
-
-    //this definitely works. Tested by running on Windows 10 and doing localhost:8080/test/100.
-
     //passing a variable through the path mapping. typing localhost:8080/test/100 will give you a white webpage that says "Variable test: var = 100"
     @GetMapping("test/{var}")
     public String getString(@PathVariable String var) {
