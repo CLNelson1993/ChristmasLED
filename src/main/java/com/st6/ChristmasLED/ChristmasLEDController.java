48a042ff7a70a3a70d14381fa0f8de0f7f2b4c99
CLNelson1993 <143137154+CLNelson1993@users.noreply.github.com>
2023-11-04 02:51:35 -0700

    Update ChristmasLEDController.java

--- src/main/java/com/st6/ChristmasLED/ChristmasLEDController.java
+++ src/main/java/com/st6/ChristmasLED/ChristmasLEDController.java
@@ -9,8 +9,7 @@ public class ChristmasLEDController {
 
     //this definitely works. Tested by running on Windows 10 and doing localhost:8080/test/100.
 
-    //going to localhost:8080/test/{var} should result in this.
-    //we are passing a variable through the path mapping. typing localhost:8080/test/100 will give you "resourceId = 100"
+    //passing a variable through the path mapping. typing localhost:8080/test/100 will give you "resourceId = 100"
     @GetMapping("test/{var}")
     public String getString(@PathVariable String var) {
         return "Variable test: var = " + var;
